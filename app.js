34f7593cc8aeb8ba7f7a1f8e74a35ead0a8db580
SMALLHORAENG <information99@naver.com>
2021-09-13 23:41:15 +0900

    21.09.13 js start variable

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
+// alert("hi")
 
+/* 2.1
+    프로그래밍에 있어서 가장 기본적인 두 가지 데이터 타입(모튼 프로그래밍)
+        1. 숫자 = integer (full number - 정수 )
+        (1 = integer / 1.5 = float )
+
+        2. 텍스트(text) = 정식명칭 string / string는 ""안에 넣어줘야 이해함
+        ("hello"+"my name is gyuseong" = "hellomy name is gyuseong" 라고 나올 것)
+        -string는 처음부터 끝까지 모두 글자로 이뤄져있다는 말이다-
+*/
+
+
+// console.log("test");
+
+// const a=5;
+// const b=2;
+// const myName ="gyuseong"
+// const veryLongVariableName = 0;
+
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a+b);
+
+// console.log("hello " + myName);
+
+/* 2.2
+    콘솔에게 메시지를 보내기
+        console.log(string or number값이 들어감)
+        괄호안의 값은 콘솔에 출력 됨
+        -작은 따옴표로 시작하면 작은 따옴표로 닫으면 사용가능-
+
+    variable(변수)
+        operation(연산)을 console에 해준다
+        console.log(5*2);
+        console.log(5/2);
+        해주면 console에 10 그리고 2.5가 출력된다
+    -js는 파일을 위에서 아래로 읽는다-
+        변수는 const(constant-상수-)를 이용하여 만들 수 있다 (바뀌지 않은 값)
+        const 변수명 = 변수에 저장할 값;
+    -값을 바꾸는데에 있어서 매우 중요한 부분-
+    -변수명을 길게 써야할 때 camelCass(낙타 등 모양)을 이용해 원래 공백부분지점 대문자로 작성-
+    ex) const veryLongVariableName = 0;
+
+    *파이썬 방식은 snake_cass(뱀 모양) very_long_variable_name = 0 방식*
+    파이썬은 js와 다르게 const와 ;를 해주지 않아도 되서 코드가 이뻐보임
+*/
+
+/* */
